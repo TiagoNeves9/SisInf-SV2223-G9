99845448d094735860ce0ba382739c99446d2ec4
TiagoNeves9 <tiagonevesx9@gmail.com>
2023-04-04 20:16:04 +0100

    update

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Tendo em conta os objetivos de aprendizagem, deverão ser produzidos os seguinte
 
 ~(a) Criar o modelo físico (1 script autónomo);~<br>
 ~(b) Remover o modelo físico (1 script autónomo);~<br>
-(c) Preenchimento inicial da base de dados (1 script autónomo);<br>
+~(c) Preenchimento inicial da base de dados (1 script autónomo);~<br>
 (d) Criar os mecanismos que permitam criar o jogador, dados os seus email e username, desativar e banir o jogador;PARCIALMENTE FEITO<br>
 (e) Criar a função totalPontosJogador que recebe como parâmetro o identificador de um jogador e devolve o número total de pontos obtidos pelo jogador.<br>
 (f) Criar a função totalJogosJogador que recebe como parâmetro o identificador de um jogador e devolve o número total de jogos diferentes nos quais o jogador participou.<br>
--- Scripts/createTables.sql
+++ Scripts/createTables.sql
@@ -1,70 +1,150 @@
+create table REGIAO
+(
+	nome_regiao VARCHAR(10),
+	primary key (nome_regiao)
+);
+
 create table JOGADORES
 (
-	id INT not null unique,
+	id_player INT not null unique,
 	email VARCHAR(30) not null unique,
 	username VARCHAR(30) not null unique,
-	primary key (id)
+	estado_player VARCHAR(30),
+	nome_regiao VARCHAR(10),
+	primary key (id_player, username, email ),
+	foreign key (nome_regiao) references REGIAO(nome_regiao)
 );
 
 create table JOGOS
 (
-	nome VARCHAR(30) not null unique,
-	ref_alfanumerica VARCHAR(10),
+	nome_game VARCHAR(30) not null unique,
+	id_game INT,
 	url VARCHAR(30),
-	primary key (nome)
+	primary key (nome_game, id_game)
 );
 
-create table PARTIDA
-(
-	nr_sequencial INT not null unique,
-	/*dt / hr de inicio e de fim por definir o tipo*/
-	primary key (nr_sequencial)
-);
-
-create table REGIAO
-(
-	nome VARCHAR(10)
-);
 
 create table CONVERSAS
 (
-	nome VARCHAR(30),
-	identificador INT,
-	primary key (identificador)
-);
-
-create table MENSAGENS
-(
-	nr_ordem INT,
-	dat VARCHAR(30),
-	hora INT,
-	texto VARCHAR(50),
-	id INT,
-	primary key (nr_ordem),
-	foreign key (id) references CONVERSAS(identificador)
+	nome_conversa VARCHAR(30),
+	id_conversa INT,
+	primary key (id_conversa)
 );
 
 create table NORMAL
 (
-	grau INT,
-	pontucao INT,
-	nrPartida INT,
-	foreign key (nrPartida) references PARTIDA(nr_sequencial)
+	nome_regiao VARCHAR(10),
+	id_game INT,
+	nome_game VARCHAR(30),
+	nmr_seq_partida INT,
+	estado_partida VARCHAR(30),
+	data_hora_inicio DATE,
+	data_hora_fim DATE,
+	pontuacao_n INT,
+	id_player INT,
+	username VARCHAR(30),
+	email VARCHAR(30),
+	grau_dificuldade INT,
+	primary key (nome_regiao, id_game, nome_game, nmr_seq_partida),
+	foreign key (nome_regiao) references REGIAO(nome_regiao),
+	foreign key (id_game, nome_game) references JOGOS(id_game, nome_game),
+	foreign key (id_player, username, email) references JOGADORES(id_player, username, email)
+	
 );
 
 create table MULTIJOGADOR
 (
-	pontuações INT,
-	nrPartida INT,
-	foreign key (nrPartida) references PARTIDA(nr_sequencial)
+	nome_regiao VARCHAR(30),
+	id_game INT,
+	nome_game VARCHAR(30),
+	nmr_seq_partida INT,
+	estado_partida VARCHAR(30),
+	data_hora_inicio DATE,
+	data_hora_fim DATE,
+	id_player INT,
+	username VARCHAR(30),
+	email VARCHAR(30),
+	primary key (nome_regiao, id_game, nome_game, nmr_seq_partida),
+	foreign key (nome_regiao) references REGIAO(nome_regiao),
+	foreign key (id_game, nome_game) references JOGOS(id_game, nome_game),
+	foreign key (id_player, username, email) references JOGADORES(id_player, username, email)
+	
+);
+
+
+create table JOGA_MJ
+(
+	nmr_seq_partida INT,
+	id_game INT,
+	nome_regiao VARCHAR(30),
+	nome_game VARCHAR(30),
+	id_player INT,
+	username VARCHAR(30),
+	email VARCHAR(30),
+	pontuacao_mj INT,
+	primary key (nmr_seq_partida, id_player, username, email, id_game, nome_regiao, nome_game),
+	foreign key (id_player, username, email) references JOGADORES(id_player, username, email),
+	foreign key (nmr_seq_partida, id_game, nome_regiao, nome_game) references MULTIJOGADOR(nmr_seq_partida, id_game, nome_regiao, nome_game)
+	
+);
+
+create table COMPRAR
+(
+	id_game INT,
+	nome_game VARCHAR(30),
+	id_player INT,
+	username VARCHAR(30),
+	email VARCHAR(30),
+	data_compra DATE,
+	preco INT,
+	primary key (id_game, nome_game, id_player, username, email),
+	foreign key (id_player, username, email) references JOGADORES(id_player, username, email),
+	foreign key (id_game, nome_game) references JOGOS(id_game, nome_game)
+	
+); 
+
+create table ESTATISTICAS_JOGO
+(
+	id_game INT,
+	nome_game VARCHAR(30),
+	total_pontos_game INT,
+	nmr_players INT,
+	nmr_partidas_game INT,
+	primary key (id_game, nome_game),
+	foreign key (id_game, nome_game) references JOGOS(id_game, nome_game)
+);
+
+
+create table ESTATISTICAS_JOGADORES
+(
+	id_player INT,
+	username VARCHAR(30),
+	email VARCHAR(30),
+	nmr_partidas_player INT,
+	nmr_jogos INT,
+	total_pontos_player INT,
+	primary key (id_player, username, email),
+	foreign key (id_player, username, email) references JOGADORES(id_player, username, email)
+);
+
+create table MENSAGENS
+(
+	id_conversa INT,
+	nmr_seq_msg INT,
+	texto VARCHAR(50),
+	remetente VARCHAR(30),
+	data_hora_msg DATE,
+	primary key (id_conversa, nmr_seq_msg),
+	foreign key (id_conversa) references CONVERSAS(id_conversa)
 );
 
 create table CRACHAS
 (
-	nome VARCHAR(30),
-	limite_pontos INT,
+	id_game INT,
+	nome_game VARCHAR(30),
+	nome_cracha VARCHAR(30),
 	url VARCHAR(30),
-	nome_jogo VARCHAR(30),
-	primary key (nome),
-	foreign key (nome_jogo) references JOGOS(nome)
+	limite_pontos INT,
+	primary key (id_game, nome_game, nome_cracha),
+	foreign key (id_game, nome_game) references JOGOS(id_game, nome_game)
 );
