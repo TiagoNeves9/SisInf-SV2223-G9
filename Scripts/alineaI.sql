d030e995e2fe776ffa621ab03136f235b7eaa8ae
A48264 - João Pereira <a48264@alunos.isel.pt>
2023-05-03 19:42:58 +0100

    Alínea I added e corrigida
    Co-Authored-By: Vasco Branco <80894056+Vaasco@users.noreply.github.com>

---
+++ Scripts/alineaI.sql
@@ -0,0 +1,49 @@
+--(i) 	Criar o procedimento armazenado iniciarConversa iniciar uma conversa (chat) dados o
+--		identificador de um jogador e o nome da conversa. O jogador deve ficar automaticamente
+--		associado à conversa. O procedimento deve devolver num parâmetro de saída o identificador
+--		da conversa criada.
+
+
+drop procedure if exists iniciarConversa;
+
+create or replace procedure iniciarConversa(id_jogador int, nome_chat varchar(30), out id_conv int)
+language plpgsql
+as $$
+declare
+    idpl int;
+    usern varchar(30);
+    email_ varchar(30);
+    nome_r varchar(30);
+begin
+    --Verificar se o id_jogador passado como parâmetro existe na tabela de jogadores.
+	if not exists(select * from jogadores where id_player = id_jogador) then
+    	raise exception 'O id passado como parâmetro não existe na tabela JOGADORES.';
+    end if;
+	
+    --Criar a nova conversa passando nome_conversa como parâmetro.
+    insert into conversas(nome_conversa) values(nome_chat);
+    
+	--Ir buscar o id da nova conversa e o seu nome e colocá-los nas variáveis.
+    select id_conversa into id_conv from conversas where nome_conversa = nome_chat;
+	--Ir buscar os dados do id_jogador passado como parâmetro.
+    select id_player, username, email, nome_regiao into idpl, usern, email_, nome_r 
+	from jogadores 
+	where id_player = id_jogador;
+	
+	--Inserir o novo valor na tabela criar.
+    insert into criar values(idpl, usern, email_, nome_r, id_conv);
+    return;
+END;
+$$;
+
+
+do $$
+declare id_chat int;
+begin
+	--call iniciarConversa(1002, 'sera?', id_chat);
+	call iniciarConversa(1001, 'chat_off_topic', id_chat);
+	RAISE NOTICE 'id_conv = %', id_chat;
+end $$;
+
+select * from conversas;
+select * from criar;
