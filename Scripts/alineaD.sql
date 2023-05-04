afd5cc74f87e0e8532d4e1708d9f3dfda30f4ed0
A48264 - João Pereira <a48264@alunos.isel.pt>
2023-05-04 02:08:56 +0100

    Mudar de função para procedure

--- Scripts/alineaD.sql
+++ Scripts/alineaD.sql
@@ -5,8 +5,9 @@
 --drop functions on the last lines of file
 
 --Criar um jogador
-create or replace FUNCTION criar_jogador(email_ VARCHAR(30) , username_ VARCHAR(30) , nome_regiao VARCHAR(30))
-returns void as $$
+create or replace procedure criar_jogador(email_ VARCHAR(30) , username_ VARCHAR(30) , nome_regiao VARCHAR(30))
+language plpgsql 
+as $$
 begin
 --Verificar se email ou o username existem na tabela de jogadores.
 	if exists(select 1 from jogadores where email = email_ or username = username_ ) then
@@ -14,13 +15,14 @@ begin
 	end if;
 --Inserir o novo jogador na tabela de jogadores.
 	insert into jogadores(email , username , estado_player , nome_regiao ) values( $1 , $2 , 'Ativo' , $3 );
-END;
-$$ LANGUAGE plpgsql;
+END
+$$;
 
 
 --Desativar um jogador
-create or replace function desativar_jogador(id_p integer)
-returns void as $$
+create or replace procedure desativar_jogador(id_p integer)
+language plpgsql
+as $$
 begin
 --Vericar se o id passado como parâmetro existe na tabela de jogador.
 	if not exists(select * from jogadores where id_player = id_p) then
@@ -29,13 +31,14 @@ begin
 --Modificar o estado do jogador em questão para "Inativo".
 	update jogadores set estado_player = 'Inativo'
 	where id_player = id_p;
-end;
-$$ language plpgsql;
+end
+$$;
 
 
 --Banir um jogador
-create or replace function banir_jogador(id_p integer)
-returns void as $$
+create or replace procedure banir_jogador(id_p integer)
+language plpgsql
+as $$
 begin
 --Vericar se o id passado como parâmetro existe na tabela de jogador.
 	if not exists(select * from jogadores where id_player = id_p) then
@@ -44,24 +47,24 @@ begin
 --Modificar o estado do jogador em questão para "Inativo".
 	update jogadores set estado_player = 'Banido'
 	where id_player = id_p;
-end;
-$$ language plpgsql;
+end
+$$;
 
 
 --Test criar_jogador
-select criar_jogador('alineaD@gmail.com', 'userAlineaD', 'Africa');
+call criar_jogador('alineaD@gmail.com', 'userAlineaD', 'Africa');
 select * from jogadores order by id_player desc limit 1;
 
 --Test desativar_jogador
-select desativar_jogador(1000);
+call desativar_jogador(1000);
 select * from JOGADORES where id_player = 1000;
 
 --Test banir_jogador
-select banir_jogador(1000);
+call banir_jogador(1000);
 select * from JOGADORES where id_player = 1000;
 
 
 --Drop functions
-drop function banir_jogador;
-drop function desativar_jogador;
-drop function criar_jogador;
+drop procedure if exists banir_jogador;
+drop procedure if exists desativar_jogador;
+drop procedure if exists criar_jogador;
