5d696e216144cd85cda5ad4777b8a41660140072
TiagoNeves9 <tiagonevesx9@gmail.com>
2023-05-05 12:03:53 +0100

    update de testes

--- Scripts/Testes.sql
+++ Scripts/Testes.sql
@@ -132,14 +132,61 @@ do
 $$
     declare id_g varchar = '9876543210';
 BEGIN
-        if pg_typeof(id_g)::text != 'varchar' then
-        raise notice 'Dados Incorretos, Teste pontosJogoPorJogador: FAIL';
-        end if;
+        /*if pg_typeof(id_g)::text = 'int' then
+        raise notice 'Dados Incorretos, Teste pontosJogoPorJogador: FAIL';  --Ainda nao sei bem como utilizar isto de forma correta
+        end if;*/
         if(select count(id_jogador) from pontosjogoporjogador(id_g)) > 0 then
             raise notice 'Teste pontosJogoPorJogador: OK';
         end if;
 ROLLBACK;
 end;
+$$;
+
+-------------------------------------
+--alinea H
+
+do
 $$
+BEGIN
+    --preenchimento da tabela com informação nova
+    call associarCracha(1004, '22222bbbbb', 'Some Sort of Rocket');
+    call associarCracha(1004, '22222bbbbb', 'Gunsmith');
 
+    if(select count(*) from  tem where id_player = 1004) = 2 then
+        raise notice 'Teste associarCracha: OK';
+    else
+        raise notice 'Teste associarCracha: FAIL';
+    end if;
+    ROLLBACK;
+END;
+$$;
 
+--------------------------------------
+--alinea I
+
+--------------------------------------
+--alinea J
+
+do
+$$
+    declare n_participants INT;
+BEGIN
+    select count(distinct id_player) into n_participants from criar where id_conversa = 100000;
+    raise notice 'A conversa com id_conversa = 100000 tem % participantes',n_participants;
+
+    call juntarConversa(1002, 100000);
+    call juntarConversa(1003, 100000);
+
+    select count(distinct id_player) into n_participants from criar where id_conversa = 100000;
+    raise notice 'A conversa com id_conversa = 100000 tem % participantes',n_participants;
+
+    if(select count(distinct id_player) from criar where id_conversa = 100000) = 4 then
+        raise notice 'Teste juntarConversa: OK';
+    else
+        raise notice 'Test juntarConversa: FAIL';
+    end if;
+
+    ROLLBACK;
+END;
+$$;
+------------------------------------------------------
